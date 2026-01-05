f920c89c823a3421ff1bf2cbf22f9fc1cd8bfccd
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2026-01-05 11:37:18 +0600

    live url

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,13 +1,13 @@
 export const getBaseUrl = () => {
-  return "http://10.10.7.103:7001/api/v1";
-  // return "https://asif7001.binarybards.online/api/v1";
+  // return "http://10.10.7.103:7001/api/v1";
+  return "https://asif7001.binarybards.online/api/v1";
   // return "https://api.thecanuckmall.ca/api/v1";
 };
 
 export const getSocketUrl = () => {
   // return "http://10.10.7.103:7001";
-  // return "https://asif7001.binarybards.online";
-  return "http://35.183.138.114:7001";
+  return "https://asif7001.binarybards.online";
+  // return "http://35.183.138.114:7001";
 };
 
 export const getImageUrl = () => {
